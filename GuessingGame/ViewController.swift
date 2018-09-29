3a1f06d908253ff0a4cc8489f7479dbc8e7a3278
Jacob Bruhn <jab599gtb@lawn-128-61-116-173.lawn.gatech.edu>
2018-09-29 14:20:37 -0400

    Initial Commit

---
+++ GuessingGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GuessingGame
+//
+//  Created by Jacob Bruhn on 9/29/18.
+//  Copyright © 2018 Jacob Bruhn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
